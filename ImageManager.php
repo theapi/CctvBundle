7d8beb02e6d7294de3cdcead7e9485a6372666e9
Peter Clarke <petermclarke@gmail.com>
2013-09-12 14:01:06 +0100

    Update ImageManager.php

--- ImageManager.php
+++ ImageManager.php
@@ -2,7 +2,6 @@
 namespace Theapi\CctvBundle;
 
 use Symfony\Component\Filesystem\Filesystem;
-
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
 
@@ -19,250 +18,311 @@ use Symfony\Component\Finder\Finder;
 class ImageManager
 {
 
-  /**
-   * The configuration array
-   */
-  protected $config;
+    /**
+     * The configuration array
+     */
+    protected $config;
 
-  /**
-   * Where the images get saved
-   */
-  protected $saveDir;
+    /**
+     * Where the images get saved
+     */
+    protected $saveDir;
 
-  /**
-   * The OutputInterface object
-   */
-  protected $output;
+    /**
+     * The OutputInterface object
+     */
+    protected $output;
 
-  /**
-   * A Symfony\Component\Process object
-   */
-  protected $process;
+    /**
+     * A Symfony\Component\Process object
+     */
+    protected $process;
 
-  /**
-   * Constructor
-   */
-  public function __construct($saveDir, $config, $process) {
-    $this->saveDir = $saveDir;
-    $this->config = $config;
-    $this->process = $process;
-  }
-
-  public function setOutput(OutputInterface $output) {
-    $this->output = $output;
-  }
-
-  public function compareDateDir($date = null, $diffThreshold = null) {
-
-    if (empty($date)) {
-      // Use the directory for today
-      $date = date('Y-m-d');
-    } else {
-      // enforce date is in the format Y-m-d
-      $date = date('Y-m-d', strtotime($date));
+    /**
+     *
+     * @param unknown_type $saveDir
+     * @param unknown_type $config
+     * @param unknown_type $process
+     */
+    public function __construct($saveDir, $config, $process)
+    {
+        $this->saveDir = $saveDir;
+        $this->config = $config;
+        $this->process = $process;
     }
 
-    $dir = $this->saveDir . '/in_' . $date;
-    if (!is_dir($dir)) {
-      throw new \Exception($dir . ' is not a directory');
-    }
-    $processedDir = $this->saveDir . '/p_' . $date;
-
-    // Compare the directories for each channel
-    $files = scandir($dir);
-    foreach ($files as $file) {
-      if ($file != '.' && $file != '..' && is_dir($dir . '/' . $file)) {
-        $sourceDir = $dir . '/' . $file;
-        $destinationDir = $processedDir . '/' . $file;
-        $this->compareDir($sourceDir, $destinationDir, $diffThreshold);
-      }
+    /**
+     *
+     * @param OutputInterface $output
+     */
+    public function setOutput(OutputInterface $output)
+    {
+        $this->output = $output;
     }
 
-  }
+    /**
+     *
+     * @param unknown_type $date
+     * @param unknown_type $diffThreshold
+     *
+     * @throws \Exception
+     */
+    public function compareDateDir($date = null, $diffThreshold = null)
+    {
 
-  public function compareDir($dir, $destinationDir, $diffThreshold = null) {
-
-    if (!is_dir($dir)) {
-      throw new \Exception($dir . ' is not a directory');
-    }
-
-    if (empty($diffThreshold)) {
-      $diffThreshold = $this->config['diff_threshold'];
-    }
-    settype($diffThreshold, 'int');
-
-    $images = array();
-    $files = scandir($dir);
-    foreach ($files as $file) {
-      if (substr($file, -4) == '.jpg') {
-        $images[] = $dir . '/' . $file;
-      }
-    }
-
-    if (empty($images)) {
-      return;
-    }
-
-    // create the destination dir
-    if (!file_exists($destinationDir)) {
-      if (!mkdir($destinationDir, 0777, true)) {
-        throw new \Exception('Unable to create Path [' . $destinationDir . ']');
-      }
-    }
-
-    $i = 0;
-    foreach ($images as $key => $image) {
-
-      // copy the first image
-      if ($key == 0) {
-        $img = trim(str_replace($dir, '', $images[$key]), '/');
-        $this->copyWithInfo($i, $dir, $destinationDir, $img);
-        $i++;
-      } else {
-        $prev = $key - 1;
-        $val = $this->compare($images[$prev], $images[$key]);
-
-        $img = trim(str_replace($dir, '', $images[$key]), '/');
-        if ($val > $diffThreshold) {
-          $text = $img . ': <info>' . $val . '</info>';
-          $this->writeln($text);
-
-          // copy image to the destination directory
-          $this->copyWithInfo($i, $dir, $destinationDir, $img);
-          $i++;
+        if (empty($date)) {
+            // Use the directory for today
+            $date = date('Y-m-d');
         } else {
-          $this->writeln($img . ': ' . $val);
+            // enforce date is in the format Y-m-d
+            $date = date('Y-m-d', strtotime($date));
         }
-      }
-    }
 
-    // Create a video of the detected images
-    $this->createVideo($destinationDir);
-  }
-
-  /**
-   * Compute how different two images are.
-   *
-   * @see http://www.imagemagick.org/Usage/compare/
-   */
-  public function compare($img_a, $img_b, $fuzz = 10) {
-    $cmd = 'compare -metric AE -fuzz ' . escapeshellarg($fuzz) . '% ' . escapeshellarg($img_a) . '  ' . escapeshellarg($img_b) . ' null: 2>&1';
-    //$process = new Process($cmd);
-    $this->process->setCommandLine($cmd);
-    $this->process->run();
-    if (!$this->process->isSuccessful()) {
-      throw new \RuntimeException($this->process->getErrorOutput());
-    }
-    $output = $this->process->getOutput();
-    return trim($output);
-  }
-
-  public function createVideo($dir) {
-    $dir = escapeshellarg($dir);
-
-    // avconv -y -v quiet -r 1 -f image2 -i img_%04d.jpg -r 25 -b 65536k a.avi
-    //$cmd = 'avconv -y -v quiet -r 1 -f image2 -i ' . $dir . '/img_%04d.jpg -r 25 -b 65536k ' . $dir . '/activity.avi';
-
-    // avconv -y -v quiet -r 1 -f image2 -i img_%04d.jpg -vcodec libx264 -preset fast -r 25 activity.mp4
-    $cmd = 'avconv -y -r 1 -f image2 -i ' . $dir . '/img_%04d.jpg -vcodec libx264 -preset fast -r 25 ' . $dir . '/activity.mp4';
-
-    $this->process->setCommandLine($cmd);
-    $this->process->setTimeout(3600);
-    $this->process->run(array($this, 'writeProcessCallbackln'));
-
-    if (!$this->process->isSuccessful()) {
-      throw new \RuntimeException($this->process->getErrorOutput());
-    }
-  }
-
-  public function getVideoFile($date = null) {
-
-    if ($date === null) {
-      // find the latest video
-      $findLatest = true;
-    }
-
-    if (empty($date)) {
-      // Use the directory for today
-      $date = date('Y-m-d');
-    } else {
-      // enforce date is in the format Y-m-d
-      $date = date('Y-m-d', strtotime($date));
-    }
-
-    $dir = $this->saveDir . '/in_' . $date;
-    if (is_dir($dir)) {
-      $processedDir = $this->saveDir . '/p_' . $date;
-      $finder = new Finder();
-      $finder->files()->in($processedDir)->name('activity.mp4');
-      foreach ($finder as $file) {
-        return $processedDir . '/' . $file->getRelativePathname();
-      }
-    }
-
-    if (!empty($findLatest)) {
-      $finder = new Finder();
-      $finder->files()->in($this->saveDir)->depth('== 2')->name('activity.mp4')->sortByModifiedTime();
-      foreach ($finder as $file) {
-        // sortByModifiedTime() returns oldest first, so get the last one
-        $path = $this->saveDir . '/' . $file->getRelativePathname();
-      }
-      if (isset($path)) {
-        return $path;
-      }
-    }
-
-    throw new \Exception('Video for ' . $date . ' not found');
-  }
-
-  public function deleteOldDirectories($processed = false) {
-
-    if (!empty($processed)) {
-      $dirPrefix = 'p_';
-    } else {
-      $dirPrefix = 'in_';
-    }
-
-    if (!empty($this->config['days_old'])) {
-      $daysOld = $this->config['days_old'];
-    } else {
-      $daysOld = 14;
-    }
-
-    $old = $daysOld * 24 * 60 * 60;
-    $nt = time();
-    $dir = $this->saveDir;
-    $files = scandir($dir);
-    foreach ($files as $file) {
-      if (substr($file, 0, strlen($dirPrefix)) == $dirPrefix) {
-        $date = str_replace($dirPrefix, '', $file);
-        $ut = strtotime($date);
-        $age = $nt - $ut;
-        if ($age > $old) {
-          $this->deleteDirectory($dir . '/' . $file);
+        $dir = $this->saveDir . '/in_' . $date;
+        if (!is_dir($dir)) {
+            throw new \Exception($dir . ' is not a directory');
         }
-      }
-    }
-  }
+        $processedDir = $this->saveDir . '/p_' . $date;
 
-  public function deleteDirectory($dir) {
-    if (is_dir($dir)) {
-      // delete the contents
-      $files = scandir($dir);
-      foreach ($files as $file) {
-        if ($file != '.' && $file != '..') {
-          if (is_dir($dir . '/' . $file)) {
-            $this->deleteDirectory($dir . '/' . $file);
-          } else {
-            unlink($dir . '/' . $file);
-          }
+        // Compare the directories for each channel
+        $files = scandir($dir);
+        foreach ($files as $file) {
+            if ($file != '.' && $file != '..' && is_dir($dir . '/' . $file)) {
+                $sourceDir = $dir . '/' . $file;
+                $destinationDir = $processedDir . '/' . $file;
+                $this->compareDir($sourceDir, $destinationDir, $diffThreshold);
+            }
         }
-      }
-      // delete directory
-      rmdir($dir);
-    }
-  }
 
+    }
+
+    /**
+     *
+     * @param unknown_type $dir
+     * @param unknown_type $destinationDir
+     * @param unknown_type $diffThreshold
+     *
+     * @throws \Exception
+     */
+    public function compareDir($dir, $destinationDir, $diffThreshold = null)
+    {
+
+        if (!is_dir($dir)) {
+            throw new \Exception($dir . ' is not a directory');
+        }
+
+        if (empty($diffThreshold)) {
+            $diffThreshold = $this->config['diff_threshold'];
+        }
+        settype($diffThreshold, 'int');
+
+        $images = array();
+        $files = scandir($dir);
+        foreach ($files as $file) {
+            if (substr($file, -4) == '.jpg') {
+                $images[] = $dir . '/' . $file;
+            }
+        }
+
+        if (empty($images)) {
+            return;
+        }
+
+        // create the destination dir
+        if (!file_exists($destinationDir)) {
+            if (!mkdir($destinationDir, 0777, true)) {
+                throw new \Exception('Unable to create Path [' . $destinationDir . ']');
+            }
+        }
+
+        $i = 0;
+        foreach ($images as $key => $image) {
+            // copy the first image
+            if ($key == 0) {
+                $img = trim(str_replace($dir, '', $images[$key]), '/');
+                $this->copyWithInfo($i, $dir, $destinationDir, $img);
+                $i++;
+            } else {
+                $prev = $key - 1;
+                $val = $this->compare($images[$prev], $images[$key]);
+
+                $img = trim(str_replace($dir, '', $images[$key]), '/');
+                if ($val > $diffThreshold) {
+                    $text = $img . ': <info>' . $val . '</info>';
+                    $this->writeln($text);
+
+                    // copy image to the destination directory
+                    $this->copyWithInfo($i, $dir, $destinationDir, $img);
+                    $i++;
+                } else {
+                    $this->writeln($img . ': ' . $val);
+                }
+            }
+        }
+
+        // Create a video of the detected images
+        $this->createVideo($destinationDir);
+    }
+
+    /**
+     * Compute how different two images are.
+     *
+     * @see http://www.imagemagick.org/Usage/compare/
+     *
+     * @param unknown_type $img_a
+     * @param unknown_type $img_b
+     * @param unknown_type $fuzz
+     *
+     * @throws \RuntimeException
+     * @return string
+     */
+    public function compare($img_a, $img_b, $fuzz = 10) {
+        $cmd = 'compare -metric AE -fuzz ' . escapeshellarg($fuzz) . '% ' . escapeshellarg($img_a) . '  ' . escapeshellarg($img_b) . ' null: 2>&1';
+        //$process = new Process($cmd);
+        $this->process->setCommandLine($cmd);
+        $this->process->run();
+        if (!$this->process->isSuccessful()) {
+            throw new \RuntimeException($this->process->getErrorOutput());
+        }
+        $output = $this->process->getOutput();
+
+        return trim($output);
+    }
+
+    /**
+     *
+     * @param unknown_type $dir
+     *
+     * @throws \RuntimeException
+     */
+    public function createVideo($dir)
+    {
+        $dir = escapeshellarg($dir);
+
+        // avconv -y -v quiet -r 1 -f image2 -i img_%04d.jpg -r 25 -b 65536k a.avi
+        //$cmd = 'avconv -y -v quiet -r 1 -f image2 -i ' . $dir . '/img_%04d.jpg -r 25 -b 65536k ' . $dir . '/activity.avi';
+
+        // avconv -y -v quiet -r 1 -f image2 -i img_%04d.jpg -vcodec libx264 -preset fast -r 25 activity.mp4
+        $cmd = 'avconv -y -r 1 -f image2 -i ' . $dir . '/img_%04d.jpg -vcodec libx264 -preset fast -r 25 ' . $dir . '/activity.mp4';
+
+        $this->process->setCommandLine($cmd);
+        $this->process->setTimeout(3600);
+        $this->process->run(array($this, 'writeProcessCallbackln'));
+
+        if (!$this->process->isSuccessful()) {
+            throw new \RuntimeException($this->process->getErrorOutput());
+        }
+    }
+
+    /**
+     *
+     * @param unknown_type $date
+     *
+     * @throws \Exception
+     * @return string
+     */
+    public function getVideoFile($date = null) {
+
+        if ($date === null) {
+            // find the latest video
+            $findLatest = true;
+        }
+
+        if (empty($date)) {
+            // Use the directory for today
+            $date = date('Y-m-d');
+        } else {
+            // enforce date is in the format Y-m-d
+            $date = date('Y-m-d', strtotime($date));
+        }
+
+        $dir = $this->saveDir . '/in_' . $date;
+        if (is_dir($dir)) {
+            $processedDir = $this->saveDir . '/p_' . $date;
+            $finder = new Finder();
+            $finder->files()->in($processedDir)->name('activity.mp4');
+            foreach ($finder as $file) {
+
+                return $processedDir . '/' . $file->getRelativePathname();
+            }
+        }
+
+        if (!empty($findLatest)) {
+            $finder = new Finder();
+            $finder->files()->in($this->saveDir)->depth('== 2')->name('activity.mp4')->sortByModifiedTime();
+            foreach ($finder as $file) {
+                // sortByModifiedTime() returns oldest first, so get the last one
+                $path = $this->saveDir . '/' . $file->getRelativePathname();
+            }
+            if (isset($path)) {
+
+                return $path;
+            }
+        }
+
+        throw new \Exception('Video for ' . $date . ' not found');
+    }
+
+    /**
+     *
+     * @param unknown_type $processed
+     */
+    public function deleteOldDirectories($processed = false) {
+
+        if (!empty($processed)) {
+            $dirPrefix = 'p_';
+        } else {
+            $dirPrefix = 'in_';
+        }
+
+        if (!empty($this->config['days_old'])) {
+            $daysOld = $this->config['days_old'];
+        } else {
+            $daysOld = 14;
+        }
+
+        $old = $daysOld * 24 * 60 * 60;
+        $nt = time();
+        $dir = $this->saveDir;
+        $files = scandir($dir);
+        foreach ($files as $file) {
+            if (substr($file, 0, strlen($dirPrefix)) == $dirPrefix) {
+                $date = str_replace($dirPrefix, '', $file);
+                $ut = strtotime($date);
+                $age = $nt - $ut;
+                if ($age > $old) {
+                    $this->deleteDirectory($dir . '/' . $file);
+                }
+            }
+        }
+    }
+
+    /**
+     *
+     * @param unknown_type $dir
+     */
+    public function deleteDirectory($dir) {
+        if (is_dir($dir)) {
+            // delete the contents
+            $files = scandir($dir);
+            foreach ($files as $file) {
+                if ($file != '.' && $file != '..') {
+                    if (is_dir($dir . '/' . $file)) {
+                        $this->deleteDirectory($dir . '/' . $file);
+                    } else {
+                        unlink($dir . '/' . $file);
+                    }
+                }
+            }
+            // delete directory
+            rmdir($dir);
+        }
+    }
+
+    /**
+     *
+     * @param unknown_type $str
+     */
     public function writeln($str)
     {
         if (!empty($this->output)) {
@@ -270,6 +330,11 @@ class ImageManager
         }
     }
 
+    /**
+     *
+     * @param unknown_type $type
+     * @param unknown_type $buffer
+     */
     public function writeProcessCallbackln($type, $buffer)
     {
         if (!empty($this->output)) {
@@ -281,6 +346,7 @@ class ImageManager
     * Get images for the given date
     *
     * @param string $date
+    *
     * @throws \Exception
     * @return string
     */
@@ -338,6 +404,13 @@ class ImageManager
         throw new \Exception('Images not found');
     }
 
+    /**
+     *
+     * @param unknown_type $id
+     *
+     * @throws Exception
+     * @return string
+     */
     public function getImage($id = null)
     {
 
@@ -357,17 +430,31 @@ class ImageManager
         }
     }
 
+    /**
+     *
+     * @param unknown_type $id
+     */
     public function getPreviousImage($id = null)
     {
 
     }
 
+    /**
+     *
+     * @param unknown_type $id
+     */
     public function getNextImage($id = null)
     {
 
     }
 
-
+    /**
+     *
+     * @param unknown_type $i
+     * @param unknown_type $source
+     * @param unknown_type $destination
+     * @param unknown_type $imgName
+     */
     protected function copyWithInfo($i, $source, $destination, $imgName)
     {
         $string = $imgName;
